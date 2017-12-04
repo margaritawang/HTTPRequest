1b00f8180ebf569d997ed420801a3deb189ba411
Margarita Amy Wang <amywang.2@alumni.ubc.ca>
2017-12-04 21:10:31 +0000

    first commit

---
+++ getHTML.js
@@ -0,0 +1,33 @@
+var https = require("https");
+
+function getHTML (options, callback) {
+
+  https.get(options, function(response) {
+
+    var arr = [];
+    response.setEncoding('utf8');
+
+    response.on('data', function(data) {
+      callback(data);
+    });
+
+    //response.on('end', function() {
+      //for (i in arr) {
+        //console.log(arr[i]);
+      //}
+    //});
+
+  });
+}
+
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions, printHTML)
