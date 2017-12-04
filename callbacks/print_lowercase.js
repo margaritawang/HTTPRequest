e1810beab905d59006bfbcdb390242c8914b4d0a
Margarita Amy Wang <amywang.2@alumni.ubc.ca>
2017-12-04 22:09:16 +0000

    step 6 first commit

---
+++ callbacks/print_lowercase.js
@@ -0,0 +1,13 @@
+var getHTML = require('../http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/lowercase.html'
+};
+
+function printLowercase (html) {
+  var lower = html.toLowerCase();
+  console.log(lower);
+}
+
+getHTML(requestOptions,printLowercase);
---
+++ callbacks/print_uppercase.js
@@ -0,0 +1,13 @@
+var getHTML = require('../http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/uppercase.html'
+};
+
+function printUppercase (html) {
+  var upper = html.toUpperCase();
+  console.log(upper);
+}
+
+getHTML(requestOptions,printUppercase);
